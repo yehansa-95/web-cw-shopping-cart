2ed6a49d79099708938f3643b83be9a24b2259c6
pasanediri97 <pasanediri@gmail.com>
2021-05-13 08:40:30 +0530

    google authentication added

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SAVE_LOCAL_USER = "SAVE_LOCAL_USER";
+export const GOOGLE_OAUTH2 = "GOOGLE_OAUTH2";
