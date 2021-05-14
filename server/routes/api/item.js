fa876189129b5823c4889fa33dbcc72b621396cb
charithakaluhendiwala <hmcnbkaluhendiwala@gmail.com>
2021-05-14 18:59:12 +0530

    bug fix in item

--- server/routes/api/item.js
+++ server/routes/api/item.js
@@ -29,23 +29,5 @@ router.get("/:id",async (req,res) => {
     }
 });
 
-router.put("/:id",async (req, res) =>{
-    try {
-        let requestedID =req.params.id;
-    let item = await Item.findById(requestedID);
-    if (!item){
-        return res.status(404).send("item you are looking for does not exist");
-    }
-
-    item.set ({qty: req.body.qty});
- 
-    item = await item.save();
-    return res.status(200).send(item);
-    } catch(ex) {
-        console.error(ex);
-        return res.status(500).send("Server Error");
-    }
-
-});
 
 module.exports = router;
